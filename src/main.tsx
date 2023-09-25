01f49a957257ef6b603e414461ef8ef5f2eb8313
Javier Ocampo <info@javierocampo.net>
2023-09-25 19:08:10 +0200

    add store

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './pages/App.tsx'
 import './assets/css/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
